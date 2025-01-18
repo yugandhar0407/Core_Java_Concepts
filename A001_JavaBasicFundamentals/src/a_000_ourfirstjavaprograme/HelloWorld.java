43757c15635af6b79a51fe0ea70a2a617c2618f9
Yugandhar Gampala <129308425+yugandhar0407@users.noreply.github.com>
2025-01-18 11:43:59 +0530

    Update HelloWorld.java

--- A001_JavaBasicFundamentals/src/a_000_ourfirstjavaprograme/HelloWorld.java
+++ A001_JavaBasicFundamentals/src/a_000_ourfirstjavaprograme/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, World.");
-		System.out.println("Hello, Java.");
+		System.out.println("Hello, Java, world.");
 		System.out.println("Hello" + " Java");
 		System.out.println("Hello "+ " Programming");
 		System.out.println("Hello World Welcome to Java Coding Practice Session");
